e635e6f5ec8e50c4389a27ea094e75aa3147d948
Adam Presley <adam@adampresley.com>
2022-07-10 14:07:34 -0500

    Basic REST + GraphQL api application template

--- internal/generators/ApiAppGenerator.go
+++ internal/generators/ApiAppGenerator.go
@@ -15,14 +15,26 @@ import (
 func ApiAppGenerator(context *answercontext.Context, localFS filesystem.FileSystem, templateFS fs.FS) {
 	dirs := []string{
 		fmt.Sprintf("%s/cmd/%s/internal/configuration", context.AppName, context.AppName),
+		fmt.Sprintf("%s/cmd/%s/internal/handlers", context.AppName, context.AppName),
+		fmt.Sprintf("%s/cmd/%s/graph/generated", context.AppName, context.AppName),
+		fmt.Sprintf("%s/cmd/%s/graph/model", context.AppName, context.AppName),
 	}
 
 	mapping := []mappings.MappingType{
 		{TemplateName: "main.go.tmpl", OutputName: filepath.Join("cmd", context.AppName, "main.go")},
 		{TemplateName: "Config.go.tmpl", OutputName: filepath.Join("cmd", context.AppName, "internal", "configuration", "Config.go")},
 		{TemplateName: "go.mod.tmpl", OutputName: "go.mod"},
+		{TemplateName: "VersionHandler.go.tmpl", OutputName: filepath.Join("cmd", context.AppName, "internal", "handlers", "VersionHandler.go")},
+		{TemplateName: "tools.go.tmpl", OutputName: filepath.Join("cmd", context.AppName, "tools.go")},
+		{TemplateName: "gqlgen.yml.tmpl", OutputName: filepath.Join("cmd", context.AppName, "gqlgen.yml")},
+		{TemplateName: "gqlgen.yml.tmpl", OutputName: filepath.Join("cmd", context.AppName, "gqlgen.yml")},
+		{TemplateName: "generated.go.tmpl", OutputName: filepath.Join("cmd", context.AppName, "graph", "generated", "generated.go")},
+		{TemplateName: "models_gen.go.tmpl", OutputName: filepath.Join("cmd", context.AppName, "graph", "model", "models_gen.go")},
+		{TemplateName: "resolver.go.tmpl", OutputName: filepath.Join("cmd", context.AppName, "graph", "resolver.go")},
+		{TemplateName: "schema.graphqls.tmpl", OutputName: filepath.Join("cmd", context.AppName, "graph", "schema.graphqls")},
+		{TemplateName: "schema.resolvers.go.tmpl", OutputName: filepath.Join("cmd", context.AppName, "graph", "schema.resolvers.go")},
 	}
 
 	dir.MakeDirs(localFS, dirs)
-	templates.Execute(localFS, templateFS, "templates/emptyapp/*.tmpl", mapping, context)
+	templates.Execute(localFS, templateFS, "templates/apiapp/*.tmpl", mapping, context)
 }
